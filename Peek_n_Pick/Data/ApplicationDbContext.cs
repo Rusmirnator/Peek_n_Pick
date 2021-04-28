a1677b450d6fdbfedfc47fd6b1f31a254f62aa9b
Marcin Rusinek <ruson80@gmail.com>
2021-04-29 00:42:10 +0200

    Add project files.

---
+++ Peek_n_Pick/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Peek_n_Pick.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peek_n_Pick.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
